2660f9868185e674f632d503973d6901561ff908
rpfozzy <fozifozzy@gmail.com>
2024-06-02 07:11:35 +0300

    Update script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        document.getElementById('loader').style.display = 'none';
-        document.getElementById('content').classList.remove('hidden');
-    }, 3000);
-});
