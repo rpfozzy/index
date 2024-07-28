4d57e7e3c6804cb50e53a4083e5fbfff22da4d3a
rpfozzy <fozifozzy@gmail.com>
2024-07-28 10:24:19 +0300

    Update rpfozzy.js

--- rpfozzy.js
+++ rpfozzy.js
@@ -1,6 +1,23 @@
-setTimeout(function() {
-            document.body.classList.add('loaded');
-            setTimeout(function() {
-                document.querySelector('.loader-wrapper').style.display = 'none';
-            }, 1000);
-        }, 4000);
+window.addEventListener('load', function() {
+    document.body.classList.add('loaded');
+    setTimeout(function() {
+        document.querySelector('.loader-wrapper').style.display = 'none';
+    }, 1000);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let progressBar = document.getElementById('progress-bar');
+    let progress = 0;
+
+    function updateProgress() {
+        progress += Math.floor(Math.random() * 5) + 1;
+        if (progress > 100) progress = 100;
+        progressBar.style.width = progress + '%';
+
+        if (progress < 100) {
+            setTimeout(updateProgress, 100);
+        }
+    }
+
+    updateProgress();
+});
